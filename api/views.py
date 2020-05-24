a4d4200cb24db41ed0e363969437f7efe12a1b54
ddeveloper72 <duncan.niamh@gmail.com>
2020-05-24 20:06:26 +0100

    added imports & class for new view

--- api/views.py
+++ api/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Movie, Rating
 
 # Create your views here.
+
+class MovieViewSet(viewsets.ModelViewSet):
