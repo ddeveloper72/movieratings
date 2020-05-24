b8a09cc6e427787ed7e2a770d0e54531cf2f605b
ddeveloper72 <duncan.niamh@gmail.com>
2020-05-24 20:11:09 +0100

    crate the RatingSerializer

--- api/views.py
+++ api/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Movie, Rating
+from .serializers import MovieSerializer, RatingSerializer
 
 # Create your views here.
 
+
 class MovieViewSet(viewsets.ModelViewSet):
+    # query everything from the movie model db
+    queryset = Movie.objects.all()
+    serializer_class = (MovieSerializer,)  #  specify tuple
+
+
+class MovieViewSet(viewsets.ModelViewSet):
+    # query everything from the movie model db
+    queryset = Rating.objects.all()
+    serializer_class = (RatingSerializer,)  # specify tuple
