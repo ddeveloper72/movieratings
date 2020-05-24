fc43afcc63b4dce88b8e3922d4e8c114c65661d8
ddeveloper72 <duncan.niamh@gmail.com>
2020-05-24 19:33:28 +0100

    create a Rating model that refrenced User from Django auth model

--- api/models.py
+++ api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -6,3 +7,9 @@ from django.db import models
 class Movie(models.Model):
     title = models.CharField(max_lenght=32)
     description = models.TextField(max_length=360)
+
+
+class Rading(models.Model):
+    movie = models.ForeignKey(Movie)
+    # include user from Django auth that made rating
+    user = models.ForeignKey(User)
