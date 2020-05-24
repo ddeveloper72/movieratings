1dd4d7c300fba13b61c854003448bb2e0422876d
ddeveloper72 <duncan.niamh@gmail.com>
2020-05-24 20:23:52 +0100

    corrected fields errors wir the ratingsserializer

--- api/serializers.py
+++ api/serializers.py
@@ -11,4 +11,4 @@ class MovieSerializer(serializers.ModelSerializer):
 class RatingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Rating
-        fields = ('id', 'title', 'description')
+        fields = ('id', 'stars', 'user', 'movie')
