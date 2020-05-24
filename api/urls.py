1b9cb0e6ac479b9527266e93602fc6ad9e0845b4
ddeveloper72 <duncan.niamh@gmail.com>
2020-05-24 20:14:07 +0100

    registered movie viewset on the url pattern

--- api/urls.py
+++ api/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path
 from rest_framework import routers
 from django.conf.urls import include
+from .views import MovieViewSet
 
 
 router = routers.DefaultRouter()
+router.register('movies', MovieViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
