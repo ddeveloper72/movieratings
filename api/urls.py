f222c22e6279a3da0cc261d43fb20d2408b4f5e8
ddeveloper72 <duncan.niamh@gmail.com>
2020-05-24 19:04:21 +0100

    added import for default router

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from django.urls import path
+from rest_framework import routers
+
+
+router = routers.DefaultRouter():
 
 urlpatterns = [
     path('new/', admin.site.urls),
