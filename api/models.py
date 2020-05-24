4c860c9807811f3e7289b98d13c597f146e0ced5
ddeveloper72 <duncan.niamh@gmail.com>
2020-05-24 19:28:23 +0100

    added model for the movie title & description

--- api/models.py
+++ api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Movie(models.Model):
+    title = models.CharField(max_lenght=32)
+    description = models.TextField(max_length=360)
