7326a0d174a5689045121f1de5597a9ff11999c8
ddeveloper72 <duncan.niamh@gmail.com>
2020-05-24 21:07:29 +0100

    building & testing custom moview viewset function

--- api/views.py
+++ api/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from .models import Movie, Rating
 from .serializers import MovieSerializer, RatingSerializer
 
@@ -10,6 +12,12 @@ class MovieViewSet(viewsets.ModelViewSet):
     # query everything from the movie model db
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+
+    # for a specific movie, True using method POST
+    @action(detail=True, methods=['POST'])
+    def rate_movie(self, request):
+        response = {'message': 'its working!'}
+        return Response(response, status=status.HTTP_200_OK)
 
 
 class RatingViewSet(viewsets.ModelViewSet):
