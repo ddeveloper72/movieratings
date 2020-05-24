014d2508001debd58e81374dc1583f9e336da23a
ddeveloper72 <duncan.niamh@gmail.com>
2020-05-24 19:35:36 +0100

    corrected spelling error

--- api/models.py
+++ api/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 
 class Movie(models.Model):
-    title = models.CharField(max_lenght=32)
+    title = models.CharField(max_length=32)
     description = models.TextField(max_length=360)
 
 
