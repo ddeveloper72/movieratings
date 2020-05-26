5078dcd7ebe311105d7cccbed01751f7cdc59bba
ddeveloper72 <duncan.niamh@gmail.com>
2020-05-26 23:16:34 +0100

    create token for new user and add to user DB

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from .models import Movie, Rating
+from rest_framework.authtoken.models import Token
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -13,6 +14,8 @@ class UserSerializer(serializers.ModelSerializer):
     #  create own definition to create user from built in crate_user function
     def crate(self, validated_data):
         user = User.objects.create_user(**validated_data)
+        # crate token for new user and add to user DB
+        Token.objects.create(user=user)
         return user
 
 
