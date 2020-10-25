cab224dd435e6091369b87e2a88eec24327f1dae
ddeveloper72 <duncan.niamh@gmail.com>
2020-10-25 20:14:44 +0000

    add home page app & path for template folders

--- movierater/settings.py
+++ movierater/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'corsheaders',
     'api',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -73,7 +74,7 @@ ROOT_URLCONF = 'movierater.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
