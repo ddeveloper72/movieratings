a23f465e664ed4bad32df2046af99c8d86f0035b
ddeveloper72 <duncan.niamh@gmail.com>
2020-11-03 22:54:32 +0000

    added static url for static root

--- movierater/urls.py
+++ movierater/urls.py
@@ -31,7 +31,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('auth/', obtain_auth_token),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
