da1a3b5bc24aacf1a87a2ea61a18c277967ea6d2
ddeveloper72 <duncan.niamh@gmail.com>
2020-11-15 21:42:09 +0000

    added serchfields to movies and ratings in admin

--- api/admin.py
+++ api/admin.py
@@ -4,5 +4,15 @@ from .models import Movie, Rating
 # Register your models here.
 
 
-admin.site.register(Movie)
-admin.site.register(Rating)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    search_fields = ('title',)
+
+
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ('movie', 'user', 'stars',)
+    search_fields = ('movie', 'user', 'stars',)
+
+
+admin.site.register(Movie, MovieAdmin)
+admin.site.register(Rating, RatingAdmin)
